bacab44b8f7a54b592454dba5a30a8fd46593d69
Mohammed Kaif Qureshi <kaif.qr1@gmail.com>
2026-09-18 23:33:02 +0530

    Fix checkout build route

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import CartPage from './pages/CartPage';
 import WishlistPage from './pages/WishlistPage';
 import NotFoundPage from './pages/NotFoundPage';
 import InfoPage from './pages/InfoPage';
-import CheckoutPage from './pages/CheckoutPage';
 
 const App: React.FC = () => (
   <BrowserRouter>
@@ -22,7 +21,6 @@ const App: React.FC = () => (
           <Route path="/shop" element={<ShopPage />} />
           <Route path="/product/:id" element={<ProductPage />} />
           <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/wishlist" element={<WishlistPage />} />
           <Route path="/info/:slug" element={<InfoPage />} />
           <Route path="*" element={<NotFoundPage />} />
